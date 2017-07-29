9194eccba15a743caa8d5c528147fb9ebc5a1cc2
Mike Wilklow <michaelwilklow@gmail.com>
2017-07-28 20:12:25 -0500

    31 - Limit Server Access in Prod

--- src/main/java/com/thistroll/server/filters/AccessControlFilter.java
+++ src/main/java/com/thistroll/server/filters/AccessControlFilter.java
@@ -1,5 +1,7 @@
 package com.thistroll.server.filters;
 
+import org.springframework.beans.factory.annotation.Required;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -9,6 +11,8 @@ import java.io.IOException;
  */
 public class AccessControlFilter implements Filter {
 
+    private String origins;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         // do nothing
@@ -17,12 +21,17 @@ public class AccessControlFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", origins);
         filterChain.doFilter(servletRequest, httpServletResponse);
     }
 
     @Override
     public void destroy() {
         // do nothing
+    }
+
+    @Required
+    public void setOrigins(String origins) {
+        this.origins = origins;
     }
 }
