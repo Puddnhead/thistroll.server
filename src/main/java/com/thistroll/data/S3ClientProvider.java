c3de45c82ba513a0484b1e7152389b2c79f61d9d
Mike Wilklow <michaelwilklow@gmail.com>
2017-07-29 17:17:56 -0500

    S3 Credentials Bug

--- src/main/java/com/thistroll/data/S3ClientProvider.java
+++ src/main/java/com/thistroll/data/S3ClientProvider.java
@@ -1,6 +1,5 @@
 package com.thistroll.data;
 
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -10,7 +9,7 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 public class S3ClientProvider {
 
     private final AmazonS3 s3Client =
-            AmazonS3ClientBuilder.standard().withCredentials(new ProfileCredentialsProvider()).build();
+            AmazonS3ClientBuilder.standard().build();
 
     public AmazonS3 getS3Client() {
         return s3Client;
