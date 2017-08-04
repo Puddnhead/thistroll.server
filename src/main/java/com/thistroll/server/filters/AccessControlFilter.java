1009d11db4adf55aeb25945743eff6d14128fdf5
Mike Wilklow <michaelwilklow@gmail.com>
2017-08-04 09:43:42 -0500

    Revert Custom Access Control Logic - *

--- src/main/java/com/thistroll/server/filters/AccessControlFilter.java
+++ src/main/java/com/thistroll/server/filters/AccessControlFilter.java
@@ -13,10 +13,6 @@ import java.util.List;
  */
 public class AccessControlFilter implements Filter {
 
-    private List<String> origins;
-
-    public static final String HEADER_NAME = "origin";
-
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         // do nothing
@@ -24,22 +20,13 @@ public class AccessControlFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        String origin = httpServletRequest.getHeader(HEADER_NAME);
-        if (origin != null && origins.contains(origin.toLowerCase())) {
-            httpServletResponse.addHeader("Access-Control-Allow-Origin", origin);
-        }
+        httpServletResponse.addHeader("Access-Control-Allow-Origin", "*");
         filterChain.doFilter(servletRequest, httpServletResponse);
     }
 
     @Override
     public void destroy() {
         // do nothing
-    }
-
-    @Required
-    public void setOrigins(List<String> origins) {
-        this.origins = origins;
     }
 }
