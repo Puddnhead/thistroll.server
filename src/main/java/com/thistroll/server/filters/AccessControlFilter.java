0d0e90a53c526000095a65adca43b26f8b8d6612
Mike Wilklow <michaelwilklow@gmail.com>
2017-08-04 08:41:43 -0500

    Fix http redirect issue - mulitple allowed origins

--- src/main/java/com/thistroll/server/filters/AccessControlFilter.java
+++ src/main/java/com/thistroll/server/filters/AccessControlFilter.java
@@ -3,15 +3,19 @@ package com.thistroll.server.filters;
 import org.springframework.beans.factory.annotation.Required;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Filter to allow cross-domain requests
  */
 public class AccessControlFilter implements Filter {
 
-    private String origins;
+    private List<String> origins;
+
+    public static final String HEADER_NAME = "origin";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -20,8 +24,12 @@ public class AccessControlFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        httpServletResponse.addHeader("Access-Control-Allow-Origin", origins);
+        String origin = httpServletRequest.getHeader(HEADER_NAME);
+        if (origins.contains(origin.toLowerCase())) {
+            httpServletResponse.addHeader("Access-Control-Allow-Origin", origin);
+        }
         filterChain.doFilter(servletRequest, httpServletResponse);
     }
 
@@ -31,7 +39,7 @@ public class AccessControlFilter implements Filter {
     }
 
     @Required
-    public void setOrigins(String origins) {
+    public void setOrigins(List<String> origins) {
         this.origins = origins;
     }
 }
