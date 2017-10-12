e5c7ac1c9d54921c458aa9efc6590648053c7195
Mike Wilklow <michaelwilklow@gmail.com>
2017-10-12 09:18:15 -0600

    224 - Critical Access-Control-Allow-Origin bug

--- src/main/java/com/thistroll/server/filters/AccessControlFilter.java
+++ src/main/java/com/thistroll/server/filters/AccessControlFilter.java
@@ -3,15 +3,17 @@ package com.thistroll.server.filters;
 import org.springframework.beans.factory.annotation.Required;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Set;
 
 /**
  * Filter to allow cross-domain requests
  */
 public class AccessControlFilter implements Filter {
 
-    private String allowedOrigins;
+    private Set<String> allowedOrigins;
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -20,8 +22,14 @@ public class AccessControlFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String origin = ((HttpServletRequest)servletRequest).getHeader("Origin");
+
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
-        httpServletResponse.addHeader("Access-Control-Allow-Origin", allowedOrigins);
+        if (allowedOrigins.contains(origin)) {
+            httpServletResponse.addHeader("Access-Control-Allow-Origin", origin);
+        } else {
+            httpServletResponse.addHeader("Access-Control-Allow-Origin", null);
+        }
         httpServletResponse.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         httpServletResponse.addHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
         httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
@@ -34,7 +42,7 @@ public class AccessControlFilter implements Filter {
     }
 
     @Required
-    public void setAllowedOrigins(String allowedOrigins) {
+    public void setAllowedOrigins(Set<String> allowedOrigins) {
         this.allowedOrigins = allowedOrigins;
     }
 }
